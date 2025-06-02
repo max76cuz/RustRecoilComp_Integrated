b5ee2bd98106ecf62bb89741c18b04d1d84b32e6
max <mkingston25@knox.nsw.edu.au>
2025-06-02 16:26:17 +1000

    Update ConfigManager.h

--- ConfigManager.h
+++ ConfigManager.h
@@ -1,3 +1,4 @@
+// ConfigManager.h
 #pragma once
 #include <string>
 #include <nlohmann/json.hpp>
@@ -8,23 +9,3 @@ public:
     static void SaveConfig(const std::string& path);
     static void LoadConfig(const std::string& path);
 };
-
-// ConfigManager.cpp
-#include "ConfigManager.h"
-#include <fstream>
-
-nlohmann::json ConfigManager::configData;
-
-void ConfigManager::SaveConfig(const std::string& path) {
-    std::ofstream file(path);
-    file << configData.dump(4);
-    file.close();
-}
-
-void ConfigManager::LoadConfig(const std::string& path) {
-    std::ifstream file(path);
-    if (file.is_open()) {
-        file >> configData;
-        file.close();
-    }
-}

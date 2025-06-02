9a38ee0cc3b2450287b4992c1b47d21d61d8c73a
max <mkingston25@knox.nsw.edu.au>
2025-06-02 16:23:59 +1000

    Update WeaponDetection.h

--- WeaponDetection.h
+++ WeaponDetection.h
@@ -1,25 +1,8 @@
+// WeaponDetection.h
 #pragma once
-#include <unordered_map>
 #include <string>
 
 class WeaponDetection {
 public:
     static std::string GetWeaponName(int weaponId);
 };
-
-// WeaponDetection.cpp
-#include "WeaponDetection.h"
-
-std::unordered_map<int, std::string> weaponIdToName = {
-    {1545779598, "Assault Rifle (AK-47)"},
-    {1588298435, "Bolt Action Rifle"},
-    {1796682209, "Custom SMG"},
-    {1318558775, "MP5A4"},
-    // Add more weapons...
-};
-
-std::string WeaponDetection::GetWeaponName(int weaponId) {
-    if (weaponIdToName.count(weaponId))
-        return weaponIdToName[weaponId];
-    return "Unknown";
-}
